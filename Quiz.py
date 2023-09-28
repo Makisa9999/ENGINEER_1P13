f254d6bfba6cc765c7c130fc27341b316be5504b
Makisa9999 <mario.n.jovanovich@gmail.com>
2023-09-28 08:43:37 -0400

    In-class quiz

--- Quiz.py
+++ Quiz.py
@@ -1,8 +1,8 @@
 import math
 
 x1 = float(input("Enter x coordinate: "))
-y1 = float(input("Enter x coordinate: "))
-x2 = float(input("Enter y coordinate: "))
+y1 = float(input("Enter y coordinate: "))
+x2 = float(input("Enter x coordinate: "))
 y2 = float(input("Enter y coordinate: "))
 
 d = math.sqrt((x2-x1)**2 + (y2-y1)**2)
