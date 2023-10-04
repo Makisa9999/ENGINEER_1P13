7ec45cfdffe0438b017e7fefe424578388484878
Makisa9999 <mario.n.jovanovich@gmail.com>
2023-10-04 17:05:39 -0400

    Update

--- Computing 2/first.py
+++ Computing 2/first.py
@@ -10,3 +10,12 @@ def averageGrade():
     return average
 
 print(averageGrade())
+
+def averageGrade1(grade1, grade2, grade3, grade4, grade5):
+    sum = 0
+    sum = grade1 + grade2 + grade3 + grade4 + grade5
+    average = sum / 5
+    return average
+
+print(averageGrade1(95,96,97,98,99))
+
